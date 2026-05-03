616c8cfe39373eeea3d1ab9544f35fe30bb40e6c
MadinaA <madina.akhrieva@audiocodes.com>
2026-05-03 23:15:44 +0300

    add comments

--- src/main/java/org/example/coursework2026marina/Launcher.java
+++ src/main/java/org/example/coursework2026marina/Launcher.java
@@ -7,5 +7,7 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.printf("Starting Education Client Application with args: %s%n", String.join(" ", args));
         Application.launch(EducationClientApplication.class, args);
+        System.out.printf("Education Client Application has exited.%n");
     }
+
 }
